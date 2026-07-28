47e052c343693ccf9549a55de853d2e8c6c15bd1
vuppalapati surya prakash <vuppalapatisurya@gmail.com>
2026-07-28 11:17:21 +0530

    Refactor: extract solver logic into solver.py

--- starter/sudoku_logic.py
+++ starter/sudoku_logic.py
@@ -1,25 +1,10 @@
-import random
-
 from board import EMPTY, SIZE, create_empty_board, deep_copy
+from solver import fill_board
 from validator import is_safe
 
-
-def fill_board(board):
-    for row in range(SIZE):
-        for col in range(SIZE):
-            if board[row][col] == EMPTY:
-                possible = list(range(1, SIZE + 1))
-                random.shuffle(possible)
-                for candidate in possible:
-                    if is_safe(board, row, col, candidate):
-                        board[row][col] = candidate
-                        if fill_board(board):
-                            return True
-                        board[row][col] = EMPTY
-                return False
-    return True
-
 def remove_cells(board, clues):
+    import random
+
     attempts = SIZE * SIZE - clues
     while attempts > 0:
         row = random.randrange(SIZE)
